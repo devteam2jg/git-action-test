97c95a774f3738045f60ee46379592ad65275f55
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 08:35:03 +0000

    testestestests

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-Script to start the application
+#Script to start the application
 
-Navigate to the application directory
+#Navigate to the application directory
 cd /home/ubuntu/project/develop/git-action-test || exit
 
 # Start the application
 echo "Starting the application..."
 # Replace 'your_application_command' with the actual command to start your application
 pnpm install
-nohup pnpm run start > /home/ubuntu/project/develop/git-action-test/app.log 2>&1 &
+nohup pnpm run start&
 
 # Check if the application started successfully
 if [ $? -eq 0 ]; then
