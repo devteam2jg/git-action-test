3c7b29c531a28653b5c6259290f92b98e29cfdf2
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 08:50:47 +0000

    testa12312451g

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,18 +3,18 @@
 #Script to start the application
 
 #Navigate to the application directory
-sudo cd /home/ubuntu/project/develop/git-action-test || exit
+cd /home/ubuntu/project/develop/git-action-test || exit
 
 # Start the application
-sudo echo "Starting the application..."
+echo "Starting the application..."
 # Replace 'your_application_command' with the actual command to start your application
-sudo pnpm install
+pnpm install
 sudo nohup pnpm run start&
 
 # Check if the application started successfully
 if [ $? -eq 0 ]; then
-    sudo echo "Application started successfully."
+    echo "Application started successfully."
 else
-    sudo echo "Failed to start the application."
-    sudo exit 1
+    echo "Failed to start the application."
+    exit 1
 fi
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Find the process running on port 3000 and terminate it
-sudo PORT=3000
+PORT=3000
 
 # Get the process ID (PID) using lsof
-sudo PID=$(lsof -t -i:$PORT)
+PID=$(lsof -t -i:$PORT)
 
 if [ -n "$PID" ]; then
-    sudo echo "Stopping process running on port $PORT with PID $PID"
-    sudo kill -9 $PID
-    sudo echo "Process terminated."
+    echo "Stopping process running on port $PORT with PID $PID"
+    kill -9 $PID
+    echo "Process terminated."
 else
-    sudo echo "No process found running on port $PORT."
+    echo "No process found running on port $PORT."
 fi
