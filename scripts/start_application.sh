545ab80affa64948ef1d6cc884235b87c960bfc8
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 09:54:10 +0000

    testestas

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -9,7 +9,7 @@ cd /home/ubuntu/project/develop/git-action-test || exit
 echo "Starting the application..."
 # Replace 'your_application_command' with the actual command to start your application
 pnpm install
-nohup pnpm run start >> /home/ubuntu/project/develop/git-action-test/scripts/scripts.log 2>&1 &
+nohup npm run start >> /home/ubuntu/project/develop/git-action-test/scripts/scripts.log 2>&1 &
 
 
 # Check if the application started successfully
