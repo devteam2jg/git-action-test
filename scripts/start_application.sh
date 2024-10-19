9664b8c678d926c5d7d3ccdb69435555eaad25ec
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 08:07:49 +0000

    testtestetst

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Script to start the application
+Script to start the application
 
-# Navigate to the application directory
-# cd /home/ubuntu/project/develop/git-action-test || exit
+Navigate to the application directory
+cd /home/ubuntu/project/develop/git-action-test || exit
 
-# # Start the application
-# echo "Starting the application..."
-# # Replace 'your_application_command' with the actual command to start your application
-# # pnpm install
-# # nohup pnpm run start /home/ubuntu/project/develop/git-action-test/app.log 2>&1 &
+# Start the application
+echo "Starting the application..."
+# Replace 'your_application_command' with the actual command to start your application
+pnpm install
+nohup pnpm run start /home/ubuntu/project/develop/git-action-test/app.log 2>&1 &
 
-# # Check if the application started successfully
-# if [ $? -eq 0 ]; then
-#     echo "Application started successfully."
-# else
-#     echo "Failed to start the application."
-#     exit 1
-# fi
+# Check if the application started successfully
+if [ $? -eq 0 ]; then
+    echo "Application started successfully."
+else
+    echo "Failed to start the application."
+    exit 1
+fi
